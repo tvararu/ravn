420759dec56c322e0d003186d60a0b526e37469b
Theodor Vararu <theo@vararu.org>
2024-06-14 14:54:39 +0200

    Replace erb component with Phlex

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
+    render Home
+  end
+
+  private
+
+  class Home < ApplicationView
+    def view_template
+      div do
+        h1(class: "font-bold text-4xl") { "Pages#home" }
+        p { "Find me in app/controllers/pages_controller.rb" }
+      end
+    end
   end
 end
