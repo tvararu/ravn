2976c3118a8b3950c0391c89fa461aa5515e66af
Theodor Vararu <theo@vararu.org>
2024-07-02 19:52:32 +0300

    Encrypt emails on users
    Also allow working with unencrypted data to allow migrating the
    production database.

--- packs/accounts/app/models/user.rb
+++ packs/accounts/app/models/user.rb
@@ -21,4 +21,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  encrypts :email, deterministic: true, downcase: true
 end
