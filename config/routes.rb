b38456db7256e6f991dbb8e4089703943b80175c
Theodor Vararu <theo@vararu.org>
2024-07-02 13:44:24 +0300

    Run rails g devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
+  devise_for :users
 
   root to: "pages#home"
+
+  get "up" => "rails/health#show", as: :rails_health_check
 end
