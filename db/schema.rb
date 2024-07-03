9187a91c1ae7fc62b06f1c9914aedd270e02a0fd
Theodor Vararu <theo@vararu.org>
2024-07-03 19:34:27 +0300

    Add Team model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_02_104240) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_03_163230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
