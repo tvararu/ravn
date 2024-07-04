77d8ab63d925a2e17f16b78cc64fbc4f19b620f5
Theodor Vararu <theo@vararu.org>
2024-07-04 12:30:26 +0300

    Make team name non-nullable

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_03_163617) do
   end
 
   create_table "teams", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- packs/accounts/app/models/team.rb
+++ packs/accounts/app/models/team.rb
@@ -3,7 +3,7 @@
 # Table name: teams
 #
 #  id         :bigint           not null, primary key
-#  name       :string
+#  name       :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
