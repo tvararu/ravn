14e5cb57caf2b7ccf05b4b4f07ab90ddff358662
Theodor Vararu <theo@vararu.org>
2024-07-07 20:40:18 +0300

    Include GOVUK helpers in base component definition
    This allows us to use things like `govuk_skip_link`.

--- app/views/components/application_component.rb
+++ app/views/components/application_component.rb
@@ -4,6 +4,12 @@ class ApplicationComponent < Phlex::HTML
   include Phlex::Rails::Helpers::Routes
   include Phlex::Rails::Helpers::T
 
+  include GovukComponentsHelper
+  include GovukLinkHelper
+  include GovukListHelper
+  include GovukSkipLinkHelper
+  include GovukVisuallyHiddenHelper
+
   unless Rails.env.production?
     def before_template
       comment { "Before #{self.class.name}" }
