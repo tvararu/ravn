a12937b6dddb654df845eda81cf41ca4385ca53c
Theodor Vararu <theo@vararu.org>
2024-07-20 22:47:39 +0300

    Add and configure Sentry
    The SENTRY_DSN is fine to keep public, but should be updated if anyone
    is using this template.

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ Replace all references to `ravn` with `myapp`:
 ```sh
 git ls-files | xargs sed -i '' 's/ravn/myapp/g'
 ```
+
+Replace `config.dsn` in `config/initializers/sentry.rb` with your Sentry DSN.
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,17 @@
+Sentry.init do |config|
+  config.dsn = "https://23f69adaa4c19f3bf865d095418da25d@o4507635847004160.ingest.de.sentry.io/4507635849101392"
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+  # Set profiles_sample_rate to profile 100%
+  # of sampled transactions.
+  # We recommend adjusting this value in production.
+  config.profiles_sample_rate = 1.0
+end
