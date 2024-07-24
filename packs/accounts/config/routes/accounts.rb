054de6bba288663a423cabe5c557d6ce716a1826
Theodor Vararu <theo@vararu.org>
2024-07-24 08:39:54 +0300

    Add customisable devise controllers
    This gives us finer control over what the devise controllers do, and
    allows us to get rid of the ERB templates.

--- packs/accounts/config/routes/accounts.rb
+++ packs/accounts/config/routes/accounts.rb
@@ -1,4 +1,11 @@
-devise_for :users
+devise_for :users, controllers: {
+                     # confirmations: "users/confirmations",
+                     # omniauth_callbacks: "users/omniauth_callbacks",
+                     # unlocks: "users/unlocks",
+                     passwords: "users/passwords",
+                     registrations: "users/registrations",
+                     sessions: "users/sessions",
+                   }
 
 get "profile", to: "users#show"
 
