c18b328cc8fd8d6c4804acb0b912fd800605d311
Theodor Vararu <theo@vararu.org>
2024-09-22 21:41:27 +0300

    Add test for .from_omniauth
    This wasn't being covered by anything.

--- packs/accounts/test/models/user_test.rb
+++ packs/accounts/test/models/user_test.rb
@@ -6,4 +6,24 @@ class UserTest < ActiveSupport::TestCase
     assert_equal "Personal", user.teams.first.name
     assert_equal true, user.memberships.first.personal
   end
+
+  test "#from_omniauth creates or finds user" do
+    auth = OmniAuth::AuthHash.new(
+      provider: "github",
+      uid: "123456",
+      info: { email: "user@example.com" },
+    )
+
+    assert_difference "User.count", 1 do
+      user = User.from_omniauth(auth)
+      assert_equal "github", user.provider
+      assert_equal "123456", user.uid
+      assert_equal "user@example.com", user.email
+      assert user.confirmed?
+    end
+
+    assert_no_difference "User.count" do
+      User.from_omniauth(auth)
+    end
+  end
 end
