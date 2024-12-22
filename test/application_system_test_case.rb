104a2072b236bc112b5a9db1188146ca14daf743
Theodor Vararu <theo@vararu.org>
2024-12-22 14:05:15 +0000

    Add sign_in helper for tests
    This will be used in system specs.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,4 +5,11 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :cuprite
 
   Capybara.default_max_wait_time = 0.5
+
+  def sign_in(user, password: "password")
+    visit new_session_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: password
+    click_on "Log in"
+  end
 end
