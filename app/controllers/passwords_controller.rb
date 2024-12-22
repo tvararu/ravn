f18854c573fcd32520396e91a4c033d98bf0f637
Theodor Vararu <theo@vararu.org>
2024-12-22 12:36:21 +0000

    Update content for password reset journeys
    Remove periods.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -13,7 +13,7 @@ class PasswordsController < ApplicationController
       PasswordsMailer.reset(user).deliver_later
     end
 
-    redirect_to new_session_path, notice: "Password reset instructions sent (if user with that email address exists)."
+    redirect_to new_session_path, notice: "Password reset instructions sent"
   end
 
   def edit
@@ -22,9 +22,10 @@ class PasswordsController < ApplicationController
 
   def update
     if @user.update(params.permit(:password, :password_confirmation))
-      redirect_to new_session_path, notice: "Password has been reset."
+      redirect_to new_session_path, notice: "Password has been reset"
     else
-      redirect_to edit_password_path(params[:token]), alert: "Passwords did not match."
+      redirect_to edit_password_path(params[:token]),
+                  alert: "Passwords did not match"
     end
   end
 
@@ -33,7 +34,8 @@ class PasswordsController < ApplicationController
   def set_user_by_token
     @user = User.find_by_password_reset_token!(params[:token])
   rescue ActiveSupport::MessageVerifier::InvalidSignature
-    redirect_to new_password_path, alert: "Password reset link is invalid or has expired."
+    redirect_to new_password_path,
+                alert: "Password reset link is invalid or has expired"
   end
 
   private
