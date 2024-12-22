151263be3d009b713c85d245ee29be515c51ab0b
Theodor Vararu <theo@vararu.org>
2024-12-22 13:33:41 +0000

    Replace password mailer ERB templates with Phlex
    This makes them easier to edit.

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -1,6 +1,23 @@
 class PasswordsMailer < ApplicationMailer
   def reset(user)
-    @user = user
-    mail subject: "Reset your password", to: user.email
+    href = edit_password_url(user.password_reset_token)
+    mail subject: "Reset your password", to: user.email,
+         body: ResetEmail.new(href:).call
+  end
+
+  private
+
+  class ResetEmail < ApplicationComponent
+    def initialize(href:)
+      @href = href
+    end
+
+    def view_template
+      p do
+        plain "You can reset your password within the next 15 minutes on "
+        a(href: @href) { "this password reset page" }
+        plain "."
+      end
+    end
   end
 end
