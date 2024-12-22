3ba4f084cff334687bef84d8d60412a2deda86f2
Theodor Vararu <theo@vararu.org>
2024-12-22 11:53:57 +0000

    Replace erb new template with phlex
    Inline it in the controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,10 @@ class SessionsController < ApplicationController
                            alert: "Try again later."
              }
 
+  use_layout TwoThirdsLayout
+
   def new
+    render New.new
   end
 
   def create
@@ -14,12 +17,32 @@ class SessionsController < ApplicationController
       start_new_session_for user
       redirect_to after_authentication_url
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      redirect_to new_session_path,
+                  alert: "The email address or password you entered is \
+                          incorrect. Please try again."
     end
   end
 
   def destroy
     terminate_session
     redirect_to new_session_path
+  end
+
+  private
+
+  class New < ApplicationComponent
+    def view_template
+      main_heading "Log in"
+
+      form_with url: session_path do |f|
+        f.govuk_email_field :email, autocomplete: "email"
+
+        f.govuk_password_field :password, autocomplete: "current-password"
+
+        f.govuk_submit "Log in"
+      end
+
+      p { link_to "Forgot password?", new_password_path }
+    end
   end
 end
