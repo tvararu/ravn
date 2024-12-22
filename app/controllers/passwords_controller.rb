d3fcd4a41ad6737af660422cab55eff38a2d8269
Theodor Vararu <theo@vararu.org>
2024-12-22 12:23:14 +0000

    Convert password reset templates to phlex
    Inlines them in PasswordsController.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,7 +2,10 @@ class PasswordsController < ApplicationController
   allow_unauthenticated_access
   before_action :set_user_by_token, only: %i[ edit update ]
 
+  use_layout TwoThirdsLayout
+
   def new
+    render New.new
   end
 
   def create
@@ -14,6 +17,7 @@ class PasswordsController < ApplicationController
   end
 
   def edit
+    render Edit.new
   end
 
   def update
@@ -30,5 +34,39 @@ class PasswordsController < ApplicationController
     @user = User.find_by_password_reset_token!(params[:token])
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     redirect_to new_password_path, alert: "Password reset link is invalid or has expired."
+  end
+
+  private
+
+  class New < ApplicationComponent
+    def view_template
+      main_heading "Forgot your password?"
+
+      form_with url: passwords_path do |f|
+        f.govuk_email_field :email,
+                            autocomplete: "email",
+                            value: helpers.params[:email]
+
+        f.govuk_submit "Email reset instructions"
+      end
+    end
+  end
+
+  class Edit < ApplicationComponent
+    def view_template
+      main_heading "Update your password"
+
+      form_with url: password_path(helpers.params[:token]), method: :put do |f|
+        f.govuk_password_field :password,
+                               autocomplete: "new-password",
+                               label: { text: "Enter new password" }
+
+        f.govuk_password_field :password_confirmation,
+                               autocomplete: "new-password",
+                               label: { text: "Repeat new password" }
+
+        f.govuk_submit "Save"
+      end
+    end
   end
 end
